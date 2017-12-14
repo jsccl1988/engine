7af306aea165defb4917f89f0e51837a8062cc73
chenchunliang <chenchunliang@baidu.com>
2017-12-14 15:35:42 +0800

    fix linux compile error

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 echo =======================build==========================
 
-workspace=$pwd
+workspace=$(pwd)
 depot_tools_path=${workspace}/src/tools/depot_tools
 python_path=${depot_tools_path}/python276_bin
 
-export PATH=${depot_tools_path}:${python_path}:${PATH}
-
-ninja -C out/Debug all
+export PATH=${PATH}:${depot_tools_path}:${python_path}
+${depot_tools_path}/ninja -C out/Debug all
--- gen.sh
+++ gen.sh
@@ -4,11 +4,11 @@ export GYP_DEFINES=component=shared_library
 export GYP_GENERATORS=ninja
 export GYP_GENERATOR_FLAGS="output_dir=../out"
 
-workspace=$pwd
+workspace=$(pwd)
 depot_tools_path=${workspace}/src/tools/depot_tools
 python_path=${depot_tools_path}/python276_bin
 
-export PATH=${depot_tools_path}:${python_path}:${PATH}
+export PATH=${PATH}:${depot_tools_path}:${python_path}
 
 source=src
 source_depth=src/build
